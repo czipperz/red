bdb8a7c202d8d0db8a12f2db3b6ad1520ac1ea8d
Chris Gregory <czipperz@gmail.com>
2019-08-20 22:13:28 -0700

    Preprocessor::create(): Initialize include_stack

--- src/preprocess.cpp
+++ src/preprocess.cpp
@@ -5,12 +5,15 @@ namespace red {
 Result Preprocessor::create(C* c, const char* cstr_file_name) {
     file_buffers.reserve(c->allocator, 1);
     file_names.reserve(c->allocator, 1);
+    include_stack.reserve(c->allocator, 1);
 
     FileBuffer buffer;
     CZ_TRY(buffer.read(cstr_file_name, c->allocator, c->allocator));
 
     file_buffers.push(buffer);
     file_names.push(cstr_file_name);
+    include_stack.push({0, 0});
+
     return Result::ok();
 }
 
