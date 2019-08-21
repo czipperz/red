96527eebe90749c40073d5baf63f7847a7588687
Chris Gregory <czipperz@gmail.com>
2019-08-20 22:35:08 -0700

    Implement Preprocessor::next() without preprocessing

--- src/preprocess.cpp
+++ src/preprocess.cpp
@@ -26,8 +26,24 @@ void Preprocessor::destroy(C* c) {
     include_stack.drop(c->allocator);
 }
 
-char Preprocessor::next(C* c, FileIndex* index) {
-    return 0;
+char Preprocessor::next(C* c, FileIndex* index_out) {
+    if (include_stack.len() == 0) {
+        *index_out = {0, file_buffers[0].len()};
+        return '\0';
+    }
+
+    FileIndex* point = &include_stack.last();
+    while (point->index == file_buffers[point->file].len()) {
+        if (include_stack.len() <= 1) {
+            *index_out = {0, file_buffers[0].len()};
+            return '\0';
+        }
+
+        include_stack.pop();
+        point = &include_stack.last();
+    }
+
+    return file_buffers[point->file].get(point->index++);
 }
 
 }
