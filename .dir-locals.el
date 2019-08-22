d9b4037e3214b1d334919d214204161a779d595c
Chris Gregory <czipperz@gmail.com>
2019-08-21 22:08:08 -0700

    Run the compiler rather than trying to run a cpp file lol

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,1 @@
-((nil . ((compile-command . "cd ~/red/build && cmake -DCMAKE_BUILD_TYPE=Debug .. >/dev/null && cmake --build . && ./test --use-colour no && ../src/compiler.cpp"))))
+((nil . ((compile-command . "cd ~/red/build && cmake -DCMAKE_BUILD_TYPE=Debug .. >/dev/null && cmake --build . && ./test --use-colour no && ./red ../src/compiler.cpp"))))
