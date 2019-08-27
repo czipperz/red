e493c35713d109b7a47939ecb67604e0bdfee689
Chris Gregory <czipperz@gmail.com>
2019-08-27 11:52:51 -0700

    Include cz/include in compiler test case

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,1 @@
-((nil . ((compile-command . "cd ~/red/build && cmake -DCMAKE_BUILD_TYPE=Debug .. >/dev/null && cmake --build . && ./test --use-colour no && ./red ../src/compiler.cpp"))))
+((nil . ((compile-command . "cd ~/red/build && cmake -DCMAKE_BUILD_TYPE=Debug .. >/dev/null && cmake --build . && ./test --use-colour no && ./red ../src/compiler.cpp -I../cz/include"))))
