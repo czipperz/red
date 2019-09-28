f9212afd3d24cdbbd171fe68be17aa5f75a2d40d
Chris Gregory <czipperz@gmail.com>
2019-09-28 07:53:53 -0700

    Extract parts of load_file

--- src/load.cpp
+++ src/load.cpp
@@ -2,6 +2,29 @@
 
 namespace red {
 
+void load_file_reserve(C* c, cpp::Preprocessor* p) {
+    c->files.buffers.reserve(c->allocator, 1);
+    c->files.names.reserve(c->allocator, 1);
+    c->files.indexes.reserve(c->allocator, 1);
+    p->file_pragma_once.reserve(c->allocator, 1);
+    p->include_stack.reserve(c->allocator, 1);
+}
+
+void load_file_push(C* c,
+                    cpp::Preprocessor* p,
+                    cz::String file_path,
+                    Hash file_path_hash,
+                    FileBuffer file_buffer) {
+    cpp::IncludeInfo info = {};
+    info.location.file = c->files.buffers.len();
+
+    c->files.buffers.push(file_buffer);
+    c->files.names.push(file_path.buffer());
+    c->files.indexes.find(file_path, file_path_hash).or_insert(info.location.file);
+    p->file_pragma_once.push(false);
+    p->include_stack.push(info);
+}
+
 Result load_file(C* c, cpp::Preprocessor* p, cz::String file_path) {
     Hash file_path_hash = StrMap<size_t>::hash(file_path);
     size_t* file_value = c->files.indexes.find(file_path, file_path_hash).value();
@@ -13,23 +36,12 @@ Result load_file(C* c, cpp::Preprocessor* p, cz::String file_path) {
             p->include_stack.push(info);
         }
     } else {
-        c->files.buffers.reserve(c->allocator, 1);
-        c->files.names.reserve(c->allocator, 1);
-        c->files.indexes.reserve(c->allocator, 1);
-        p->file_pragma_once.reserve(c->allocator, 1);
-        p->include_stack.reserve(c->allocator, 1);
+        load_file_reserve(c, p);
 
         FileBuffer file_buffer;
         CZ_TRY(file_buffer.read(file_path.buffer(), c->allocator));
 
-        cpp::IncludeInfo info = {};
-        info.location.file = c->files.buffers.len();
-
-        c->files.buffers.push(file_buffer);
-        c->files.names.push(file_path.buffer());
-        c->files.indexes.find(file_path, file_path_hash).or_insert(info.location.file);
-        p->file_pragma_once.push(false);
-        p->include_stack.push(info);
+        load_file_push(c, p, file_path, file_path_hash, file_buffer);
     }
 
     return Result::ok();
--- src/load.hpp
+++ src/load.hpp
@@ -6,6 +6,13 @@
 
 namespace red {
 
+void load_file_reserve(C* c, cpp::Preprocessor* p);
+void load_file_push(C* c,
+                    cpp::Preprocessor* p,
+                    cz::String file_path,
+                    Hash file_path_hash,
+                    FileBuffer file_buffer);
+
 Result load_file(C* c, cpp::Preprocessor* p, cz::String file_path);
 
 }
