57b7c7bd393e7c5758f9cac7a5ea591a20226e64
Chris Gregory <czipperz@gmail.com>
2020-04-08 15:21:43 -0400

    Replace red::Str_Map with cz::Str_Map

--- src/files.hpp
+++ src/files.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
 #include <cz/allocator.hpp>
+#include <cz/str_map.hpp>
 #include <cz/vector.hpp>
-#include "str_map.hpp"
 
 namespace red {
 struct File;
 
 struct Files {
     cz::Vector<File> files;
-    red::StrMap<size_t> indexes;
+    cz::Str_Map<size_t> indexes;
 
     void destroy(cz::Allocator allocator);
 };
